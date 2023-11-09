c0360e08b056196bd9291678ab87370da75ea678
Aldo Wiranata <aldowiranata16@gmail.com>
2023-11-09 16:19:10 +0700

    JS_dasar.orenda

---
+++ js/video6_0.js
@@ -0,0 +1,10 @@
+// var nama = prompt("Input your name :");
+// console.log('Hello ' + nama);
+
+var nama = prompt('Input Your Name :');
+alert('Start');
+for (var i = 0; i < 5; i++) {
+    alert('Hello ' + nama);
+    console.log('Hello ' + nama);
+}
+alert('Finish');
