25b38c65299b205fc9b28142e9d7942410573753
Aldo Wiranata <aldowiranata16@gmail.com>
2023-11-10 09:36:30 +0700

    JS_dasar.orenda

---
+++ js/video6_2.js
@@ -0,0 +1,14 @@
+//jawaban video 6,1
+// var na = 1;
+// while (na <= 10) {
+//     console.log('Angkot no ' + na + ' beroperasi dengan baik');
+//     na++
+// }
+
+//upgrade
+var jmlAngkot = parseInt(prompt('Input jumlah angkot'));
+var noAngkot = 1;
+while (noAngkot <= jmlAngkot) {
+    console.log('Angkot no ' + noAngkot + ' beroperasi dengan baik');
+    noAngkot++
+}
