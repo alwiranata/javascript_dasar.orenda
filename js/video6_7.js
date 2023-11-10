39d85aea17b77eece79fc1c23cfd4ce5c4797e39
Aldo Wiranata <aldowiranata16@gmail.com>
2023-11-10 11:37:14 +0700

    JS_dasar.orenda

---
+++ js/video6_7.js
@@ -0,0 +1,53 @@
+// var angka = parseInt(prompt('Masukkan angka'));
+// if (angka === 1) {
+//     console.log('Angka yang anda masukkan ' + angka)
+// } else if (angka === 2) {
+
+//     console.log('Angka yang anda masukkan ' + angka)
+// } else if (angka === 3) {
+//     console.log('Angka yang anda masukkan salah' + angka)
+// } else {
+//     alert('Angka yang anda masukkan salah')
+// }
+
+// var angka = parseInt(prompt('Masukkan angka'));
+// switch (angka) {
+//     case 1:
+//         alert('anda memasukkan angka 1');
+//         break;
+//     case 2:
+//         alert('anda memasukkan angka 2');
+//         break;
+//     case 3:
+//         alert('anda memasukkan angka 3');
+//         break;
+//     default:
+//         alert('angka yang anda masukkan salah');
+//         break
+// }
+
+// var item = prompt('Masukkan nama makanan/minuman : \n (cth: nasi,daging dan burger)');
+// switch (item) {
+//     case 'nasi':
+//         alert('makanan/minuman SEHAT')
+//         break;
+//     case 'daging':
+//         alert('makanan/minuman SEHAT')
+//         break;
+//     case 'burger':
+//         alert('makanan/minuman penuh kalori')
+//         break;
+// }
+
+//upgrade
+var item = prompt('Masukkan nama makanan/minuman : \n (cth: nasi,daging dan burger)');
+switch (item) {
+    case 'nasi':
+    case 'daging':
+        alert('makanan/minuman SEHAT')
+        break;
+    case 'burger':
+        alert('makanan/minuman penuh kalori')
+        break;
+}
+
