eee83f9a3a080c996a68babbe96cbd77d9e08153
Aldo Wiranata <aldowiranata16@gmail.com>
2023-11-11 19:34:05 +0700

    JS_dasar.orenda

---
+++ js/video8_0.js
@@ -0,0 +1,26 @@
+// var binatang = ['kucing', 'kelinci', 'gajah',];
+
+// console.log(binatang[2])
+
+
+//memastikan tipe object
+// var binatang;[];
+// binatang = ['aldo', 'wiranata',];
+// console.log(typeof (binatang));
+
+//element dimulai dari 1 sedangkan index dari 0
+// var binatang;[];
+// binatang = ['aldo', 'wiranata',];
+// console.log(binatang.length);
+
+//element pada array
+
+var hari = ['senin', 'selasa', 'rabu'];
+var siswa = ['aldo', 'wiranata', '16'];
+var angka = [10, 123, 1234];
+var myArr = ['teks', 10, true];
+var myFunc = function () {
+    alert('Hello World');
+}
+var myArr2 = ['aldo', 12, true, myFunc];
+var myArr3 = ['aldo', 12, true, myFunc, [1, 2, 3]];
